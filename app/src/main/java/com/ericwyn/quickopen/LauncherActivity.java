ed65f0fd316e333d904eac5fbb6325c8fbe036ea
Ericwyn <Ericwyn.chen@gmail.com>
2019-10-08 22:27:32 +0800

    long press tile to set the quick open app, mproved application startup speed

--- app/src/main/java/com/ericwyn/quickopen/LauncherActivity.java
+++ app/src/main/java/com/ericwyn/quickopen/LauncherActivity.java
@@ -1,13 +1,22 @@
 package com.ericwyn.quickopen;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,14 +24,50 @@ import java.util.List;
 public class LauncherActivity extends AppCompatActivity {
 
     private ListView lv_app_list;
-//    private AppAdapter mAppAdapter;
+    private AppAdapter mAppAdapter;
     public Handler mHandler = new Handler();
+
+    public SharedPreferences sharedPreferences;
+    public SharedPreferences.Editor editor;
+
+    public Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_launcher);
+//        getActionBar().setTitle("请选择想要启动的应用");
 
+        setTitle("选择要启动的应用");
+        sharedPreferences = getSharedPreferences("QuickOpen", Context.MODE_PRIVATE);
+
+        editor = sharedPreferences.edit();
+
+        setContentView(R.layout.activity_launcher);
+        lv_app_list = (ListView) findViewById(R.id.lv_app_list);
+        mAppAdapter = new AppAdapter();
+        lv_app_list.setAdapter(mAppAdapter);
+        lv_app_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                TextView pgNameTextView = (TextView) view.findViewById(R.id.tv_app_pg_ame);
+                TextView appNameTextView = (TextView) view.findViewById(R.id.tv_app_name);
+
+                editor.putString("appName", pgNameTextView.getText().toString());
+                editor.apply();
+
+                if (toast == null){
+                    toast = Toast.makeText(LauncherActivity.this, "启动应用设置为: " + appNameTextView.getText().toString(), Toast.LENGTH_LONG);
+                } else {
+                    toast.cancel();
+                    toast = Toast.makeText(LauncherActivity.this, "启动应用设置为: " + appNameTextView.getText().toString(), Toast.LENGTH_LONG);
+//                    toast.setText("启动应用设置为:" + appNameTextView.getText());
+//                    toast.setDuration(Toast.LENGTH_LONG);
+                }
+                toast.show();
+
+            }
+        });
+        initAppList();
 
     }
 
@@ -39,7 +84,11 @@ public class LauncherActivity extends AppCompatActivity {
 //                continue;
 //            }
                 MyAppInfo myAppInfo = new MyAppInfo();
-                myAppInfo.setAppName(packageInfo.packageName);
+//                myAppInfo.setAppName(packageInfo.packageName);
+//                ApplicationInfo appInfo = ctx.getApplicationInfo();
+//                appName = (String) packManager.getApplicationLabel(appInfo);
+                myAppInfo.setPackageName(packageInfo.packageName);
+                myAppInfo.setAppName((String) packageManager.getApplicationLabel(packageInfo.applicationInfo));
                 if (packageInfo.applicationInfo.loadIcon(packageManager) == null) {
                     continue;
                 }
@@ -52,6 +101,85 @@ public class LauncherActivity extends AppCompatActivity {
         return myAppInfos;
     }
 
+    private void initAppList(){
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                //扫描得到APP列表
+                final List<MyAppInfo> appInfos = scanLocalInstallAppList(LauncherActivity.this.getPackageManager());
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mAppAdapter.setData(appInfos);
+                    }
+                });
+            }
+        }.start();
+    }
+
+
+    class AppAdapter extends BaseAdapter {
+
+        List<MyAppInfo> myAppInfos = new ArrayList<MyAppInfo>();
+
+        public void setData(List<MyAppInfo> myAppInfos) {
+            this.myAppInfos = myAppInfos;
+            notifyDataSetChanged();
+        }
+
+        public List<MyAppInfo> getData() {
+            return myAppInfos;
+        }
+
+        @Override
+        public int getCount() {
+            if (myAppInfos != null && myAppInfos.size() > 0) {
+                return myAppInfos.size();
+            }
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            if (myAppInfos != null && myAppInfos.size() > 0) {
+                return myAppInfos.get(position);
+            }
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder mViewHolder;
+            MyAppInfo myAppInfo = myAppInfos.get(position);
+            if (convertView == null) {
+                mViewHolder = new ViewHolder();
+                convertView = LayoutInflater.from(getBaseContext()).inflate(R.layout.item_app_info, null);
+                mViewHolder.iv_app_icon = (ImageView) convertView.findViewById(R.id.iv_app_icon);
+                mViewHolder.tx_app_name = (TextView) convertView.findViewById(R.id.tv_app_name);
+                mViewHolder.tx_app_pk_name = (TextView)convertView.findViewById(R.id.tv_app_pg_ame);
+                convertView.setTag(mViewHolder);
+            } else {
+                mViewHolder = (ViewHolder) convertView.getTag();
+            }
+            mViewHolder.iv_app_icon.setImageDrawable(myAppInfo.getImage());
+            mViewHolder.tx_app_name.setText(myAppInfo.getAppName());
+            mViewHolder.tx_app_pk_name.setText(myAppInfo.getPackageName());
+            return convertView;
+        }
+
+        class ViewHolder {
+
+            ImageView iv_app_icon;
+            TextView tx_app_name;
+            TextView tx_app_pk_name;
+        }
+    }
 
 
 }
--- app/src/main/java/com/ericwyn/quickopen/QuickOpenService.java
+++ app/src/main/java/com/ericwyn/quickopen/QuickOpenService.java
@@ -1,40 +1,75 @@
 package com.ericwyn.quickopen;
 
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.IBinder;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class QuickOpenService extends TileService {
 
-    private String appName = "com.noname81.lmt";
+//    private String appName = "com.noname81.lmt";
     private PackageManager packageManager;
-    private Context context;
+//    private Context context;
 
     @Override
     public void onCreate() {
-        super.onCreate();
-        this.context = getApplicationContext();
-//        packageManager = getPackageManager();
+//        super.onCreate();
+//        this.context = getApplicationContext();
+        packageManager = getPackageManager();
     }
 
     @Override
     public void onClick() {
-        getQsTile().setState(Tile.STATE_INACTIVE);
         if (packageManager == null){
-            if (context != null){
-                packageManager = context.getPackageManager();
-                Intent launchIntentForPackage = packageManager.getLaunchIntentForPackage(appName);
-                startActivity(launchIntentForPackage);
-            }
+            return;
+        }
+        getQsTile().setState(Tile.STATE_UNAVAILABLE);
+
+        SharedPreferences sharedPreferences= getSharedPreferences("QuickOpen", Context .MODE_PRIVATE);
+        String appName = sharedPreferences.getString("appName", "null");
+        if (appName.equals("null")){
+            Toast.makeText(getApplicationContext(), "尚未设置启动应用，请长按进入设置页面", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // 新的启动方法
+        Intent intent = new Intent(Intent.ACTION_MAIN);//入口Main
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);// 启动LAUNCHER,跟MainActivity里面的配置类似
+        intent.setPackage(appName);//包名
+        //查询要启动的Activity
+        List<ResolveInfo> apps = packageManager.queryIntentActivities(intent, 0);
+        if (apps.size() > 0) {//如果包名存在
+            ResolveInfo ri = apps.get(0);
+            // //获取包名
+            String packageName = ri.activityInfo.packageName;
+            //获取app启动类型
+            String className = ri.activityInfo.name;
+            //组装包名和类名
+            ComponentName cn = new ComponentName(packageName, className);
+            //设置给Intent
+            intent.setComponent(cn);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            //根据包名类型打开Activity
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "找不到包名;" + appName, Toast.LENGTH_SHORT).show();
         }
 
     }
 
     @Override
     public void onTileAdded() {
-        getQsTile().setState(Tile.STATE_INACTIVE);
+        getQsTile().setState(Tile.STATE_UNAVAILABLE);
     }
+
+
 }
